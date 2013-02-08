13fad898b473267e26fd8462d261636f524a6eac
Jelte Fennema <jelte12345@gmail.com>
2013-02-08 14:04:26 +0000

    Add slightly optimized version

--- 1/triangle_rasterization/trirast.c
+++ 1/triangle_rasterization/trirast.c
@@ -56,6 +56,31 @@ void draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
     float fa = fij(x0, y0, x1, y1, x2, y2);
     float fb = fij(x1, y1, x2, y2, x0, y0);
     float fc = fij(x2, y2, x0, y0, x1, y1);
+    for (float y = ymin; y <= ymax; y++) {
+        for (float x = xmin; x <= xmax; x++) {
+            float a = fij(x, y, x1, y1, x2, y2) / fa;
+            float b = fij(x, y, x2, y2, x0, y0) / fb;
+            float c = fij(x, y, x0, y0, x1, y1) / fc;
+            if (a >= 0 && b >= 0 && c >= 0 &&
+                    (a > 0 || fij(-1, -1, x1, y1, x2, y2) * fa > 0) &&
+                    (b > 0 || fij(-1, -1, x2, y2, x0, y0) * fb > 0) &&
+                    (c > 0 || fij(-1, -1, x0, y0, x1, y1) * fc > 0)) {
+                PutPixel(x, y, r, g, blue);
+            }
+        }
+    }
+
+}
+
+void draw_triangle_optimized(float x0, float y0, float x1, float y1, float x2,
+        float y2, byte r, byte g, byte blue) {
+    float xmin = floor(lowest(x0, x1, x2));
+    float ymin = floor(lowest(y0, y1, y2));
+    float xmax = floor(highest(x0, x1, x2));
+    float ymax = floor(highest(y0, y1, y2));
+    float fa = fij(x0, y0, x1, y1, x2, y2);
+    float fb = fij(x1, y1, x2, y2, x0, y0);
+    float fc = fij(x2, y2, x0, y0, x1, y1);
     float fa_outside = fij(-1, -1, x1, y1, x2, y2) * fa > 0;
     float fb_outside = fij(-1, -1, x2, y2, x0, y0) * fb > 0;
     float fc_outside = fij(-1, -1, x0, y0, x1, y1) * fc > 0;
@@ -72,9 +97,5 @@ void draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
         }
     }
 
-}
-
-void draw_triangle_optimized(float x0, float y0, float x1, float y1, float x2,
-        float y2, byte r, byte g, byte b) {
 
 }
